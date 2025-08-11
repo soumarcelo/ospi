6666dfde408a72c60cbb8076a7f4525ad6672f6b
Marcelo Santos <santosmarcelo.dev@gmail.com>
2025-08-11 13:59:36 -0300

    feat: adds mockup to authorization service

---
+++ Engine/Infrastructure/Services/AuthorizationService.cs
@@ -0,0 +1,18 @@
+﻿using Engine.Application.Interfaces;
+using Engine.Domain.ValueObjects;
+
+namespace Engine.Infrastructure.Services;
+
+public class AuthorizationService : IAuthorizationService
+{
+    public Task<IList<AuthorizationPermission>> GetUserPermissions(Guid userId)
+    {
+        IList<AuthorizationPermission> permissions =
+        [
+            new("PaymentAccountBalance.Read"),
+            new("PaymentAccountStatement.Read"),
+        ];
+
+        return Task.FromResult(permissions);
+    }
+}
